99de343076823bd0420fdf262fe6a3f30f5ee952
ActiveEvil <active_evil@protonmail.com>
2018-12-31 13:45:40 +0000

    use location

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,30 +1,35 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
+import { Location } from '@reach/router';
 import style from './style.module.css'
 
 class Nav extends React.Component {
   render() {
     const { items } = this.props
     return (
-      <nav>
-        <ul className={style.list}>
-          {items.map(item => (
-            <li key={item.node.id} className={style.item}>
-              <Link
-                to={item.node.route}
-                className={`${style.link} ${
-                  window.location.pathname === item.node.route
-                    ? style.isActive
-                    : ''
-                }`}
-              >
-                {item.node.page}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
+      <Location>
+        {({ location }) => (
+          <nav>
+            <ul className={style.list}>
+              {items.map(item => (
+                <li key={item.node.id} className={style.item}>
+                  <Link
+                    to={item.node.route}
+                    className={`${style.link} ${
+                      location.pathname === item.node.route
+                        ? style.isActive
+                        : ''
+                    }`}
+                  >
+                    {item.node.page}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        )}
+       </Location>
     )
   }
 }
